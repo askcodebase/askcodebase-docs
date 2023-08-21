b164f823a5ef794713abab7487cb8e69dfb79a81
梅世祺 <shiqi.mei@outlook.com>
2023-08-21 11:47:23 +0800

    feat: update metadata

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,36 +2,33 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span className='text-lg font-bold'>OpenQuickJS Docs</span>,
+  logo: <span className='text-lg font-bold'>AskCodebase Docs</span>,
   project: {
-    link: 'https://github.com/OpenQuickJS/quickjs'
+    link: 'https://github.com/jipitiai/askcodebase-docs'
   },
-  docsRepositoryBase: 'https://github.com/OpenQuickJS/docs',
+  docsRepositoryBase: 'https://github.com/jipitiai/askcodebase-docs',
   head: (
     <>
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <meta property='og:title' content='OpenQuickJS Docs' />
+      <meta property='og:title' content='AskCodebase Docs' />
       <meta
         property='og:description'
-        content='A community-driven QuickJS fork. Issues and Pull Requests are welcome.'
+        content='Your AI programming autopilot.'
       />
-      <title>OpenQuickJS Docs</title>
+      <title>AskCodebase Docs</title>
       <link rel='icon' type='image/png' href='/favicon.svg' />
     </>
   ),
-  nextThemes: {
-    defaultTheme: 'light'
-  },
   footer: {
     component: null
   },
   editLink: {
-    text: 'https://github.com/OpenQuickJS/docs',
+    text: 'https://github.com/jipitiai/askcodebase-docs',
     component({ children: _children, className, filePath }) {
       return (
         <a
           className='nx-text-xs nx-font-medium nx-text-gray-500 hover:nx-text-gray-900 dark:nx-text-gray-400 dark:hover:nx-text-gray-100 contrast-more:nx-text-gray-800 contrast-more:dark:nx-text-gray-50'
-          href={`https://github.com/OpenQuickJS/docs/edit/main/${filePath}`}
+          href={`https://github.com/jipitiai/askcodebase-docs/edit/main/${filePath}`}
           target='_blank'
         >
           Edit this page
